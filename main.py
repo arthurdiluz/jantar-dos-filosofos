f26327bfcbd3c8076480d9306d8949f84acd1de0
Arthur Diniz <adl.csrss.exe1999@gmail.com>
2019-07-02 20:09:01 -0300

    this shit works and doesn't work at the same time

--- main.py
+++ main.py
@@ -10,47 +10,49 @@ Se falhar ao pegar o segundo garfo, o primeiro garfo é largado e os garfos são
 
 
 class Filosofo(threading.Thread):
-    running = True
+    executando = True  # Thread começa executando
 
+    # Construtor da classe Filosofo
     def __init__(self, nome, garfo_esquerda, garfo_direita):
-        threading.Thread.__init__(self)
+        threading.Thread.__init__(self)  # Herança dos atributos da classe Thread
         self.nome = nome
         self.garfo_esquerda = garfo_esquerda
         self.garfa_direita = garfo_direita
-        self.comeu = False
 
+    # Sobrescrita do método "run" da classe "Thread".
     def run(self):
-        while self.running:
-            # Filósofo está pensando (dormindo).
-            # time.sleep(5)
+        while self.executando:
+            print(F">{self.nome} está pensando")
             time.sleep(random.uniform(5, 15))
-            print(F"{self.nome} está pensando!")
             self.comer()
 
     def comer(self):
+        '''
+        Pega o garfo 1 e tenta pegar o garfo 2. Se o garfo 2 estiver livre,
+        o ele janta e solta os dois garfos em seguida, caso contrário,
+        ele desiste de comer e continua pensando.
+        '''
+
         garfo1, garfo2 = self.garfo_esquerda, self.garfa_direita
 
-        while self.running:
+        while self.executando:
             garfo1.acquire(True)
             locked = garfo2.acquire(False)
             if locked:
                 break
             garfo1.release()
-
-            # print(F"{self.nome} parou de comer")
-            # garfo1, garfo2 = garfo2, garfo1
         else:
             return
 
+        print(F">{self.nome} começou a comer")
         self.jantando()
-        garfo2.release()
         garfo1.release()
+        garfo2.release()
 
     def jantando(self):
-        print(F"{self.nome} começou a comer")
-        # time.sleep(random.uniform(1, 10))
-        time.sleep(5)
-        print(F"{self.nome} parou de comer")  # ao terminar de comer
+        time.sleep(random.uniform(1, 10))
+        # time.sleep(5)
+        print(F">{self.nome} parou de comer")
 
 
 if __name__ == '__main__':
@@ -64,12 +66,12 @@ if __name__ == '__main__':
     mesa = [Filosofo(nomes[i], garfos[i % 5], garfos[(i + 1) % 5]) for i in range(5)]
 
     for _ in range(2):
-        print("start")
-        Filosofo.running = True
+        print("Começo da janta" + '\n')
+        Filosofo.executando = True
 
         for filosofo in mesa:
             filosofo.start()
 
-        time.sleep(100)
-        Filosofo.running = False
-        print("end")
+        time.sleep(random.uniform(5, 15))
+        Filosofo.executando = False
+        print('\n' + "Fim da Janta")
