1f0acd32f15a633eacb53c04373b5d5af6a255c5
Arthur Diniz <adl.csrss.exe1999@gmail.com>
2019-08-06 10:58:09 -0300

    apresentação

--- main.py
+++ main.py
@@ -2,24 +2,22 @@ from threading import Thread, Lock
 from random import seed, uniform
 from time import sleep
 '''
-5 filósofos e 5 garfos; é necessário 3 garfos para comer;
-Deadlock é evitado para nunca esperar um garfo com outro garfo na mão;
-"Procedure" é bloquear ao esperar o primeiro garfo e desbloquear ao pegar o segundo garfo.
-Se falhar ao pegar o segundo garfo, o primeiro garfo é largado e os garfos são trocados.
+5 filósofos e 5 garfos; é necessário 2 garfos para comer
+Deadlock é evitado para nunca esperar um garfo com outro garfo na mão
+Se falhar ao pegar o segundo garfo, o primeiro garfo é largado e os garfos são trocados
 '''
 
 
 class Filosofo(Thread):
-    executando = True  # Thread começa executando
+    executando = True  # começa executando para
 
-    # Construtor da classe Filosofo
     def __init__(self, nome, garfo_esquerda, garfo_direita):
-        Thread.__init__(self)  # Herança dos atributos da classe Thread
+        Thread.__init__(self)
         self.nome = nome
         self.garfo_esquerda = garfo_esquerda
         self.garfa_direita = garfo_direita
 
-    # Sobrescrita do método "run" da classe "Thread".
+    # Sobrescrita de Thread.
     def run(self):
         while self.executando:
             print(F"> {self.nome} está pensando")
@@ -35,13 +33,13 @@ class Filosofo(Thread):
 
         garfo1, garfo2 = self.garfo_esquerda, self.garfa_direita
         while self.executando:
-            garfo1.acquire(True)
-            locked = garfo2.acquire(False)
+            garfo1.acquire(True)  # tenta pegar o primeiro garfo
+            locked = garfo2.acquire(False)  # verifica se o segundo garfo está disponível
             if locked:
                 break
             garfo1.release()
         else:
-            return
+            return  # se nao for possível comer
 
         print(F" >{self.nome} começou a comer")
         sleep(uniform(5, 10))
@@ -51,13 +49,9 @@ class Filosofo(Thread):
 
 
 if __name__ == '__main__':
-    # número base para gerar valores aleatórios
     seed(507129)
-    # nome dos filósofos
     nomes = ("Arthur", "Jesus", "VitorV", "Dijkstra", "Neymar")
-    # garfos com lock para impedir que dois acessem o mesmo garfo
     garfos = [Lock() for _ in range(5)]
-    # mesa com 5 filósofos e 1 garfo para cada
     mesa = [Filosofo(nomes[i], garfos[i % 5], garfos[(i + 1) % 5]) for i in range(5)]
 
     for _ in range(50):
